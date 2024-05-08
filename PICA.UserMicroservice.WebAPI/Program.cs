3fa19ff8785320ca65f6f7f2c1a83ade0f0a1bdf
Jhon Lagos <jhonlagosa@gmail.com>
2024-05-08 00:00:17 -0500

    Ajustes en los logs

--- PICA.UserMicroservice.WebAPI/Program.cs
+++ PICA.UserMicroservice.WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using PICA.UserMicroservice.WebAPI.Config;
 using PICA.UserMicroservice.WebAPI.Interfaces;
 using PICA.UserMicroservice.WebAPI.Repositories;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,18 +11,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSerilog();
 
 builder.Configuration.AddEnvironmentVariables(prefix: "APPCONF_");
 
 // Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+// Logs
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-var loggerFactory = app.Services.GetService<ILoggerFactory>();
-loggerFactory.AddFile(builder.Configuration["Logging:LogFilePath"]!.ToString());
-
 // Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseSwagger();
 app.UseSwaggerUI();
